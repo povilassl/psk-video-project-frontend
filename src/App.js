7ebbf34d3eb9a2fbcaa8967b1ae3a80a52b8ed78
Rosita Raišuotytė <76535712+RositaRai@users.noreply.github.com>
2023-03-22 22:13:03 +0200

    testing-pipeline-xd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+//NERADAU KUR KA GALECIAU PAKEISTI:D
+//TESTUOJU, PS NIEKAS MAN NEPATINKA CIA, NIEKO NESUPRANTU :DD
+
 function App() {
   return (
     <div className="App">
