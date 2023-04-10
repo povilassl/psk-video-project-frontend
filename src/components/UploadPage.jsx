efbf4dbf2d77db4952a8b18d0a41a7129fb2a9d9
JustasBan <justas.ban2001@gmail.com>
2023-04-10 16:52:09 +0300

    env and disable while uploading

--- src/components/UploadPage.jsx
+++ src/components/UploadPage.jsx
@@ -8,34 +8,48 @@ export function UploadPage() {
     const [videoFile, setVideoFile] = useState(null);
     const [thumbnailImage, setThumbnailImage] = useState(null);
     const [uploadState, setUploadState] = useState('');
-  
+
     const handleSubmit = async (event) => {
-      event.preventDefault();
-  
-      // Check file size and format constraints
-      if (!videoName || !username || !description || !videoFile || !thumbnailImage) {
-        setUploadState('failed');
-        return;
-      }
-      if (videoFile.size > 10 * 1024 * 1024 || !/\.mp4$/i.test(videoFile.name)) {
-        setUploadState('failed');
-        return;
-      }
-      if (thumbnailImage.size > 1 * 1024 * 1024 || !/\.(jpe?g|png)$/i.test(thumbnailImage.name)) {
-        setUploadState('failed');
-        return;
-      }
-  
-      // Upload video
-      setUploadState('uploading');
-      try {
-        await uploadVideo(videoName, username, description, videoFile, thumbnailImage);
-        setUploadState('success');
-      } catch (error) {
-        setUploadState('failed');
-        console.log(error);
-      }
+
+        const videoFileSizeLimit = 10e6; // 10 MB
+        const imageFileSizeLimit = 1e6; // 1 MB
+        
+        event.preventDefault();
+
+        // Check file size and format constraints
+        if (!videoName || !username || !description || !videoFile || !thumbnailImage) {
+            setUploadState('failed');
+            return;
+        }
+        if (videoFile.size > videoFileSizeLimit || !/\.mp4$/i.test(videoFile.name)) {
+            setUploadState('failed');
+            return;
+        }
+        if (thumbnailImage.size > imageFileSizeLimit || !/\.(jpe?g|png)$/i.test(thumbnailImage.name)) {
+            setUploadState('failed');
+            return;
+        }
+
+        // Upload video
+        setUploadState('uploading');
+        try {
+            await uploadVideo(videoName, username, description, videoFile, thumbnailImage);
+            setUploadState('success');
+
+            // Reset form, if successful
+            setVideoName('');
+            setUsername('');
+            setDescription('');
+            setVideoFile(null);
+            setThumbnailImage(null);
+        } catch (error) {
+            setUploadState('failed');
+            console.log(error);
+        }
     };
+
+    //disable upload button while uploading, to prevent spamming
+    const isFormDisabled = uploadState === 'uploading';
 
     return (
         <div>
@@ -54,6 +68,7 @@ export function UploadPage() {
                         type="text"
                         value={videoName}
                         onChange={(e) => setVideoName(e.target.value)}
+                        disabled={isFormDisabled}
                     />
                 </label>
                 <br />
@@ -63,6 +78,7 @@ export function UploadPage() {
                         type="text"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
+                        disabled={isFormDisabled}
                     />
                 </label>
                 <br />
@@ -71,6 +87,7 @@ export function UploadPage() {
                     <textarea
                         value={description}
                         onChange={(e) => setDescription(e.target.value)}
+                        disabled={isFormDisabled}
                     />
                 </label>
                 <br />
@@ -80,6 +97,7 @@ export function UploadPage() {
                         type="file"
                         accept="video/*"
                         onChange={(e) => setVideoFile(e.target.files[0])}
+                        disabled={isFormDisabled}
                     />
                 </label>
                 <br />
@@ -89,10 +107,11 @@ export function UploadPage() {
                         type="file"
                         accept="image/*"
                         onChange={(e) => setThumbnailImage(e.target.files[0])}
+                        disabled={isFormDisabled}
                     />
                 </label>
                 <br />
-                <button type="submit">Upload Video</button>
+                <button type="submit" disabled={isFormDisabled}>Upload Video</button>
             </form>
         </div>
     );
