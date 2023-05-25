d7a746e299a335ec91e9ca2e1d980e6f44d94a57
Simutyte <rugile.simutyte@gmail.com>
2023-05-25 17:30:42 +0300

    user profile page and and the ability to change information

--- src/components/UserPage/Profile.jsx
+++ src/components/UserPage/Profile.jsx
@@ -1,7 +1,143 @@
+import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+import { getUserInfo, updateUserInfo } from "../../services/user_endpoints/userInteractions"
+import "../../css/UsersPages/profilePage.css";
+
+ 
 export const Profile = () => {
+    const [user, setUser] = useState({
+        username: '',
+        email: '',
+        firstName: '',
+        lastName: '',
+        lastInfoUpdateDateTime: '',
+        state: null
+    });
+
+    
+    const handleErrorMsg = (errorMsg) => {
+        if(errorMsg === 'BadEmail')
+            notifyError("Your email is not valid");
+        else if(errorMsg === 'BadUsername')
+            notifyError("Your username is not valid");
+        else if(errorMsg === 'BadFirstName')
+            notifyError("Your first name is not valid");
+        else if(errorMsg === 'BadLastName')
+            notifyError("Your last name is not valid");
+        else
+            notifyError("errorMsg");
+    }
+    
+
+    const notifyError = (message) => toast.error(message);
+
+    useEffect(() => {
+        console.log("bandymas");
+        getUserInfo()
+            .then((response) => {
+                setUser({...user, username: response.data.username,
+                                  email: response.data.emailAddress,
+                                  firstName: response.data.firstName,
+                                  lastName: response.data.lastName,
+                                  lastInfoUpdateDateTime: response.data.lastInfoUpdateDateTime})
+            })
+            .catch((error) => {
+                setUser({...user, state: "failed"})
+            });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, []);
+
+    const handleUpdateInfo = () => {
+        updateUserInfo(user.username, user.email, user.firstName, 
+                       user.lastName, user.lastInfoUpdateDateTime)
+                        .then((response) => {
+                            setUser({ ...user, state: "success" })
+                            // console.log(response)
+                        })
+                        .catch((error) => {
+                            if(error.response.status === 400)
+                            {
+                                handleErrorMsg(error.response.data)
+                                setUser({ ...user, state: "data failed" })
+                            }    
+                            else if(error.response.status === 409) //jei koinfliktas
+                            {   
+                                notifyError(error.response.data);
+                                setUser({ ...user, state: "conflict" })
+                                // console.log(error.response)
+                            }
+                            else
+                            {
+                                setUser({ ...user, state: "data failed" })
+                                // console.log(error.response)
+                            }
+                        })     
+    };
+        
     return (
-        <div>
-            <h1>Profile</h1>
+        <div className="videoProfileDiv">
+            {user.state === "failed" && notifyError("something went wrong while uploading your data")}
+            <div className="videoProfileCard">
+                <img src="https://ionicframework.com/docs/img/demos/avatar.svg" 
+                     className="profileLogo"
+                     alt="avatar"/>
+                <h1>Change your profile</h1>
+                <div className="profileInputSection">
+                    <form className='profileForm'>
+                        <div className="profileFormGroup">
+                            <input type="text" 
+                                   className="profileInput" 
+                                   placeholder="Username" 
+                                   onChange={(e) => setUser({ ...user, username: e.target.value })}
+                                   value= {user.username}
+                                   pattern='^[a-zA-Z0-9]{5,19}$'
+                                   title='you can only use letters and numbers'
+                            />
+                            <i className="profileInputIcon uil uil-user"></i>
+                        </div>
+                        <div className="profileFormGroup">
+                            <input type="email" 
+                                   className="profileInput" 
+                                   placeholder="Email" 
+                                   onChange={(e) => setUser({ ...user, email: e.target.value })}
+                                   value={user.email}
+                                   pattern='^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+                                />
+                            <i className="profileInputIcon uil uil-at"></i>
+                        </div>
+                                            
+                        
+                        <div className="profileFormGroup">
+                            <input type="text" 
+                                   className="profileInput" 
+                                   placeholder="Name" 
+                                   onChange={(e) => setUser({ ...user, firstName: e.target.value })}
+                                   value={user.firstName} 
+                                   pattern='^[a-zA-Z]{0,49}$'
+                                   title='you can only use letters'
+                            />
+                            <i className="profileInputIcon uil uil-user"></i>
+                        </div>
+                        <div className="profileFormGroup">
+                            <input type="text" 
+                                   className="profileInput" 
+                                   placeholder="Surname" 
+                                   onChange={(e) => setUser({ ...user, lastName: e.target.value })}
+                                   value={user.lastName}
+                                   pattern='^[a-zA-Z]{0,49}$'
+                                   title='you can only use letters'
+                            />
+                            <i className="profileInputIcon uil uil-user"></i>
+                        </div>
+                        <button className="profileBtn" 
+                                // type="button"
+                                type="submit" 
+                                onClick={handleUpdateInfo}
+                        >Submit changes</button>
+                    </form>
+                </div>
         </div>
+    </div>
+
     )
 }
